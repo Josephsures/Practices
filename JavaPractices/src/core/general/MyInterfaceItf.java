01415bd47a588566379d115fee641e49aeded1c4
Josephsures <josephsuresh2011@gmail.com>
2017-04-07 01:06:21 +0530

    Revert "updating"
    This reverts commit 5da65c9987e30511ac8c00097cedb29d8403ac1c.

--- JavaPractices/src/core/general/MyInterfaceItf.java
+++
@@ -1,7 +0,0 @@
-package core.general;
-
-public interface MyInterfaceItf {
-	public void method1();
-	public String val1="SOMEVAL";
-    int a=10;
-}
--- JavaPractices/src/multithreading/ThreadSleepMethod1.java
+++
@@ -1,9 +0,0 @@
-package multithreading;
-class TestSleepMethod1 extends Thread{  
- public void run(){  
-  for(int i=1;i<5;i++){  
-    try{Thread.sleep(500);}catch(InterruptedException e){System.out.println(e);}  
-    System.out.println(i);  
-  }  
- }  
-}  
